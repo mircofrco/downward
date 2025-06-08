d5b6c879a39570cff6a8f6d62fea051279b89497
mircofrco <m.franco@stud.unibas.ch>
2025-06-08 17:22:46 +0200

    Add new experiment 250609-standard-experiment.py to see if div-, criteria- and the normal tiebreaking_openlist work the same and see if there is any overhead

---
+++ run_experiments/250609-standard-experiment.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import common_setup
+from common_setup import IssueConfig, IssueExperiment
+
+import os
+
+from lab.reports import Attribute
+from lab.reports import arithmetic_mean
+
+from downward.reports.absolute import AbsoluteReport
+
+from lab.environments import LocalEnvironment, BaselSlurmEnvironment
+from experimentparser import PlateauParser
+
+# TODO: Enter git commit hash of code version you want to use.
+REVISIONS = ["c3a3593fcf9eea648a62d4ec51bdc608a2560804"]
+DRIVER_OPTIONS = ["--overall-time-limit", "30m"]
+CONFIGS = [
+    common_setup.IssueConfig(
+        "tiebreaking_openlist (fifo)",
+        [
+            "--search",
+            "let(h, lmcut(), eager(tiebreaking([sum([g(), h]), h], unsafe_pruning=false), reopen_closed=true, f_eval=sum([g(), h])))",
+        ],
+        driver_options=DRIVER_OPTIONS,
+    ),
+    common_setup.IssueConfig(
+        "criteria_tiebreaking_openlist (fifo)",
+        [
+            "--search",
+            "let(h, lmcut(), eager(criteria_tiebreaking([sum([g(), h]), h], unsafe_pruning=false, tiebreaking_criteria=fifo), reopen_closed=true, f_eval=sum([g(), h])))",
+        ],
+        driver_options=DRIVER_OPTIONS,
+    ),
+    common_setup.IssueConfig(
+        "div_tiebreaking_openlist (fifo)",
+        [
+            "--search",
+            "let(h1, lmcut(), eager(div_tiebreaking([sum([g(), h1]), h1], unsafe_pruning=false, tiebreaking_criteria=fifo), reopen_closed=true, f_eval=sum([g(), h1])))",
+        ],
+        driver_options=DRIVER_OPTIONS,
+    ),
+    common_setup.IssueConfig(
+        "criteria_tiebreaking_openlist (lifo)",
+        [
+            "--search",
+            "let(h, lmcut(), eager(criteria_tiebreaking([sum([g(), h]), h], unsafe_pruning=false, tiebreaking_criteria=lifo), reopen_closed=true, f_eval=sum([g(), h])))",
+        ],
+        driver_options=DRIVER_OPTIONS,
+    ),
+    common_setup.IssueConfig(
+        "div_tiebreaking_openlist (lifo)",
+        [
+            "--search",
+            "let(h1, lmcut(), eager(div_tiebreaking([sum([g(), h1]), h1], unsafe_pruning=false, tiebreaking_criteria=lifo), reopen_closed=true, f_eval=sum([g(), h1])))",
+        ],
+        driver_options=DRIVER_OPTIONS,
+    ),
+    # TODO: Add your configs.
+]
+
+BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
+REPO_DIR = os.environ["DOWNWARD_REPO"]
+
+#if common_setup.is_test_run():
+#    SUITE = common_setup.IssueExperiment.DEFAULT_TEST_SUITE
+#    ENVIRONMENT = LocalEnvironment(processes=2)
+#else:
+SUITE = common_setup.DEFAULT_OPTIMAL_SUITE
+ENVIRONMENT = BaselSlurmEnvironment(
+    # Choose between infai_1 or infai_2. Either works but stick to a
+    # single partition when results have to be comparable.
+    partition="infai_2",
+    # TODO: Change this to your own email, you will get notified when your
+    # jobs finish.
+    email="m.franco@stud.unibas.ch",
+    export=["PATH", "DOWNWARD_BENCHMARKS"],
+    )
+
+exp = common_setup.IssueExperiment(
+    # TODO: Adapt this path. It should probably be "../.." if you put this
+    # script into experiments/something in the downward root directory (same
+    # as the Downward Lab tutorial).
+    REPO_DIR,
+    revisions=REVISIONS,
+    configs=CONFIGS,
+    environment=ENVIRONMENT,
+)
+
+exp.add_suite(BENCHMARKS_DIR, SUITE)
+
+exp.add_parser(exp.EXITCODE_PARSER)
+exp.add_parser(exp.SINGLE_SEARCH_PARSER)
+exp.add_parser(exp.PLANNER_PARSER)
+exp.add_parser(PlateauParser())
+
+entries = Attribute("max_entries_per_plateau", min_wins=False, function=arithmetic_mean, absolute=False)
+depth = Attribute("max_depth", min_wins=False, function=arithmetic_mean, absolute=False)
+width = Attribute("max_depth_width", min_wins=False, function=arithmetic_mean, absolute=False)
+number_of_plateaus = Attribute("max_leveled_plateaus", min_wins=False, function=arithmetic_mean, absolute=False)
+ATTRIBUTES = IssueExperiment.DEFAULT_TABLE_ATTRIBUTES + [entries, depth, width, number_of_plateaus]
+
+exp.add_step("build", exp.build)
+exp.add_step("start", exp.start_runs)
+exp.add_step("parse", exp.parse)
+exp.add_fetcher(name="fetch")
+exp.add_report(AbsoluteReport(attributes=ATTRIBUTES))
+
+exp.run_steps()
